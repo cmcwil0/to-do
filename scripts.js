c3c8694a55f0504962873f5f5b47396208abbeee
cmcwil0 <cmcwil0@gmail.com>
2025-07-30 11:35:34 -0500

    Shows user error if value already in list
    Last commit was accidently an old file

--- scripts.js
+++ scripts.js
@@ -1,45 +1,43 @@
-let priority = '!'
+let priority = 'low';
 
 
-const priorityButton = document.querySelector(".priorityButton");
+const priorityButton = document.querySelector('.priorityButton');
 
-priorityButton.addEventListener('click', function() { //updates priority button text 
-    let value = priorityButton.textContent;
-    if (value === '!') {
-        priorityButton.textContent = '!!';
-        priority = '!!';
-    } else if(value === '!!') {
-        priorityButton.textContent = '!!!';
-        priority = '!!!';
-    } else if(value === '!!!') {
+priorityButton.addEventListener('click', () => { //changes button text on click and updates variable
+    if(priorityButton.textContent.length < 3) {
+        priorityButton.textContent += '!';
+        priority = priorityButton.textContent === '!!' ? 'medium' : 'high';
+    } else { 
         priorityButton.textContent = '!';
-        priority = '!';
-    }
-
+        priority = 'low';
+     }
 });
 
-function getListInfo(priority) { //gets lists as arrays and
-    let list = [];
-    if(priority === '!') { //grabs array by priority
-        list = Array.from(document.querySelector('.lowPriorityList ul')).querySelectorAll('li');
-    } else if (priority === '!!') {
-        list = Array.from(document.querySelector('.mediumPriorityList ul')).querySelectorAll('li');
-    } else if (priority === '!!!') {
-        list = Array.from(document.querySelector('.highPriorityList ul')).querySelectorAll('li');
-    }
-    
-    
-    // for(let i = 0; i < list.length; i++) { //iterates through array to check for duplicates
-    //     for(let j = 0; j < list.length ; j++) {
-    //         if(list[i] === list[j]) {
+const inputBox = document.querySelector('.inputBox');
+const userMessage = document.querySelector('.userMessage');
 
-    //         }
-    //     }
-    // }
+function submitTask(task, priority) {
+    let list = document.querySelectorAll('.' + priority + 'Priority li'); //grabs Node list by priority to be iterated
+    userMessage.style.visibility = 'hidden'; //hides text incase there was error displayed before
+
+    let isRunning = true;
+    
+    list.forEach(element => {
+        if (element.textContent === task) {
+            userMessage.textContent = '*Already in list*';
+            userMessage.style.visibility = 'visible';
+            return isRunning = false;
+        }
+    });
+    if (isRunning === false) { return; } // stops function here if value is already in list
+
+    const newTask = document.createElement('li'); //creates new list element
+    newTask.textContent = task;
+    list = document.querySelector('.' + priority + 'Priority'); //changes list to ul element to append to
+
+    list.appendChild(newTask); //adds task to list
+    
 }
 
-
-function submit(input, priority) {
-    
-    
-}
+const submitButton = document.querySelector('.submitButton');
+submitButton.addEventListener('click', () => submitTask(inputBox.value, priority));
